1bbd723a41f6b1be100e067adc289c58471110c5
Arkadiusz Zając <ArekZc@gmail.com>
2021-08-21 13:08:38 +0200

    connect api with ui

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { ApiController } from './api.controller';
+import { ApiService } from './api.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'public')
+    })
+  ],
+  controllers: [ApiController],
+  providers: [ApiService],
 })
 export class AppModule {}
